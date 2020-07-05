d6a65c60dd3deb928ecdd340dcd8379d6eeee3b5
Gayathriamirtharaj <gayathriamirtharaj@gmail.com>
2020-07-05 08:55:56 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactive-forms';
+  userForm; 
+  userDetails=[];
+  constructor(private fb:FormBuilder) {
+    
+    this.userForm = this.fb.group({
+      'Name' : this.fb.control('',[Validators.required,Validators.minLength(5),Validators.maxLength(20)]),
+      'Mail-id':this.fb.control('',[Validators.required,Validators.maxLength(250),Validators.minLength(10),(Validators.pattern('[a-z0-9.@]*'))]),
+      'Date-of-Birth':this.fb.control(''),
+      'age' : this.fb.control('',[Validators.required,Validators.min(18),Validators.max(55)]),
+      'Gender':this.fb.control('',Validators.required),
+      'Marital Status':this.fb.control(''),
+      'FavFood':this.fb.control(''),
+
+      'country' : this.fb.control('IN'),
+      'address' : this.fb.array([
+        this.fb.group({
+          'streetname' : this.fb.control('',[Validators.required]),
+          'Doorno':this.fb.control(''),
+          'zipcode' : this.fb.control('')
+        })
+      ])
+    })
+    console.log(this.userForm);
+  }
+  submitForm(){
+    if(this.userForm.valid){
+       console.log(this.userForm.value);
+       this.userDetails.push(this.userForm.value);
+    }
+   }
+  
+ 
+
 }
